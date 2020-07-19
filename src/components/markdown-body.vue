d209e42060fa6cf0912a6a7af094e94e26be4f7b
Xuefeng <i@oiuv.cn>
2020-07-19 15:51:44 +0800

    update prismjs & package

--- src/components/markdown-body.vue
+++ src/components/markdown-body.vue
@@ -4,14 +4,6 @@
 
 <script>
 import Prism from 'prismjs'
-import 'prismjs/components/prism-markup'
-import 'prismjs/components/prism-markup-templating'
-import 'prismjs/components/prism-php'
-import 'prismjs/components/prism-javascript'
-import 'prismjs/components/prism-css'
-import 'prismjs/components/prism-sass'
-import 'prismjs/components/prism-scss'
-import 'prismjs/components/prism-json'
 
 export default {
   name: 'markdown-body',
