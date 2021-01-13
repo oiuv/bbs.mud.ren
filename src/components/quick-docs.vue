190b2528866ca17bde354c9a92cf509d877945b3
Xuefeng <i@oiuv.cn>
2021-01-13 20:05:20 +0800

    update quick-docs

--- src/components/quick-docs.vue
+++ src/components/quick-docs.vue
@@ -1,12 +1,20 @@
 <template>
   <div class="box text-gray-50">
     <div class="box-heading d-flex align-items-center justify-content-between">
-      <div class="text-13">MUD文档</div>
+      <div class="text-13">MUD资源</div>
     </div>
     <ul>
       <li class="py-1">
         <docs-icon class="mr-1"/>
-        <a class="text-gray-50" href="https://www.mud.ren/lpc.html" target="_blank">LPC函数文档</a>
+        <a class="text-gray-50" href="https://bbs.mud.ren/threads/98">MUD资源汇总</a>
+      </li>
+      <li class="py-1">
+        <docs-icon class="mr-1"/>
+        <a class="text-gray-50" href="https://wiki.mud.ren/index.php?title=Lpc:Efun" target="_blank">LPC函数WIKI</a>
+      </li>
+      <li class="py-1">
+        <docs-icon class="mr-1"/>
+        <a class="text-gray-50" href="https://www.mud.ren/lpc.html" target="_blank">LPC函数文档（旧）</a>
       </li>
       <li class="py-1">
         <docs-icon class="mr-1"/>
@@ -18,11 +26,31 @@
       </li>
       <li class="py-1">
         <docs-icon class="mr-1"/>
-        <a class="text-gray-50" href="https://github.com/mudren" target="_blank">MUDLIB大全</a>
+        <a class="text-gray-50" href="https://github.com/mudren" target="_blank">MUDLIB下载</a>
       </li>
       <li class="py-1">
         <docs-icon class="mr-1"/>
         <a class="text-gray-50" href="https://im-mortal.cn/mudlist" target="_blank">中文MUD列表</a>
+      </li>
+      <li class="py-1">
+        <docs-icon class="mr-1"/>
+        <a class="text-gray-50" href="https://www.revivalworld.org/mud/taiwanmudlist" target="_blank">台湾MUD列表</a>
+      </li>
+      <li class="py-1">
+        <docs-icon class="mr-1"/>
+        <a class="text-gray-50" href="https://www.topmudsites.com/" target="_blank">国外MUD列表</a>
+      </li>
+      <li class="py-1">
+        <docs-icon class="mr-1"/>
+        <a class="text-gray-50" href="http://mud.ren/websocket.html" target="_blank">MUD WebSocket Test</a>
+      </li>
+      <li class="py-1">
+        <docs-icon class="mr-1"/>
+        <a class="text-gray-50" href="http://mud.ren:8080/" target="_blank">勇者斗恶龙MUD</a>
+      </li>
+      <li class="py-1">
+        <docs-icon class="mr-1"/>
+        <a class="text-gray-50" href="http://mud.ren:8888/" target="_blank">炎黄群侠传MUD</a>
       </li>
     </ul>
   </div>
--- src/modules/home/home.vue
+++ src/modules/home/home.vue
@@ -27,7 +27,7 @@
       <div class="col-md-3">
         <guest-login-guide class="mb-2" />
         <quick-docs class="mb-2" />
-        <hot-tags />
+        <!-- <hot-tags /> -->
         <user-ranking class="mt-2" />
         <new-users class="mt-2" />
       </div>
