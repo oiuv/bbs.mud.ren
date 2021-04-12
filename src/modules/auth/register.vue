d20303b8a427efc18fa535a37bb4029109f1e11a
Xuefeng <i@oiuv.cn>
2021-04-12 11:43:27 +0800

    百度统计

--- src/modules/auth/register.vue
+++ src/modules/auth/register.vue
@@ -15,7 +15,7 @@
               @blur="validateEmail"
               required
             >
-            <small id="emailHelp" class="form-text text-muted">提示：<a href="http://mud.ren:8000" target="_blank">炎黄MUD</a>玩家无需注册，可直接使用游戏账号登录</small>
+            <small id="emailHelp" class="form-text text-muted">提示：<a href="http://mud.ren:8888" target="_blank">炎黄MUD</a>和<a href="http://mud.ren:8080" target="_blank">DQMUD</a>玩家可直接使用游戏账号登录</small>
           </div>
           <div class="form-group">
             <label>用户名</label>
