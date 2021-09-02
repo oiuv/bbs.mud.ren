a92d53f32ce4b24732a9d9e58dd03d9077e181b6
Xuefeng <i@oiuv.cn>
2021-09-02 16:15:30 +0800

    调整banner为随机显示1条

--- src/components/banner.vue
+++ src/components/banner.vue
@@ -1,12 +1,22 @@
 <template>
   <div :id="id" class="carousel slide" data-ride="carousel" v-if="banner">
-    <ol class="carousel-indicators" v-if="banner.banners.length > 1">
-      <li :data-target="idSelector" :data-slide-to="i - 1" class="active" v-for="i in banner.banners.length" :key="i"></li>
-    </ol>
+    <!-- <ol class="carousel-indicators" v-if="banner.banners.length > 1">
+      <li
+        :data-target="idSelector"
+        :data-slide-to="i - 1"
+        class="active"
+        v-for="i in banner.banners.length"
+        :key="i"
+      ></li>
+    </ol> -->
     <div class="carousel-inner">
-      <div class="carousel-item active" v-for="item of banner.banners" :key="item.id">
+      <div
+        class="carousel-item active"
+        v-for="item of banner.banners"
+        :key="item.id"
+      >
         <a :href="item.url || 'javascript:;'" target="_blank">
-          <img class="d-block w-100" :src="item.image_url" alt="First slide">
+          <img class="d-block w-100" :src="item.image_url" alt="First slide" />
           <div class="carousel-caption" v-if="item.title || item.description">
             <h1 v-if="item.title">{{ item.title }}</h1>
             <p v-if="item.description">{{ item.description }}</p>
@@ -14,58 +24,77 @@
         </a>
       </div>
     </div>
-    <a class="carousel-control-prev" :href="idSelector" role="button" data-slide="prev" v-if="banner.banners.length > 1">
+    <!-- <a
+      class="carousel-control-prev"
+      :href="idSelector"
+      role="button"
+      data-slide="prev"
+      v-if="banner.banners.length > 1"
+    >
       <span class="carousel-control-prev-icon" aria-hidden="true">
         <arrow-left></arrow-left>
       </span>
       <span class="sr-only">Previous</span>
     </a>
-    <a class="carousel-control-next" :href="idSelector" role="button" data-slide="next" v-if="banner.banners.length > 1">
+    <a
+      class="carousel-control-next"
+      :href="idSelector"
+      role="button"
+      data-slide="next"
+      v-if="banner.banners.length > 1"
+    >
       <span class="carousel-control-next-icon" aria-hidden="true">
         <arrow-right></arrow-right>
       </span>
       <span class="sr-only">Next</span>
-    </a>
+    </a> -->
   </div>
 </template>
 
 <script>
-import ArrowLeft from '$icons/ArrowLeft'
-import ArrowRight from '$icons/ArrowRight'
+import ArrowLeft from "$icons/ArrowLeft";
+import ArrowRight from "$icons/ArrowRight";
 
 export default {
-  name: 'Banner',
+  name: "Banner",
   components: { ArrowLeft, ArrowRight },
   props: {
     name: {
       type: String,
-      required: true
-    }
+      required: true,
+    },
   },
   computed: {
-    id () {
-      return 'banner-' + this.name
+    id() {
+      return "banner-" + this.name;
     },
-    idSelector () {
-      return '#' + this.id
-    }
+    idSelector() {
+      return "#" + this.id;
+    },
   },
-  data () {
+  data() {
     return {
-      banner: null
-    }
+      banner: null,
+    };
   },
   methods: {
-    loadBanner () {
-      this.$http.get('banners/' + this.name).then(banner => {
-        this.banner = banner
-      }).catch(err => {})
-    }
+    loadBanner() {
+      this.$http
+        .get("banners/" + this.name)
+        .then((banner) => {
+          // 随机取一条banner
+          banner.banners = [
+            banner.banners[Math.floor(Math.random() * banner.banners.length)],
+          ];
+          this.banner = banner;
+        })
+        .catch((err) => {});
+    },
   },
-  mounted () {
-    this.loadBanner()
-  }
-}
+  mounted() {
+    this.loadBanner();
+  },
+};
 </script>
 
 <style lang="scss">
